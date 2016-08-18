bf76e4af0a83cbe10a52350e657a5d8f37383e03
MichaelC <m@michaelcullum.com>
2016-08-18 12:26:18 +0100

    Add voting validity unit test

--- tests/StvElectionRunnerTest.php
+++ tests/StvElectionRunnerTest.php
@@ -17,7 +17,30 @@ class StvElectionRunnerTest extends \PHPUnit_Framework_TestCase
         $logger = $this->createMock(Logger::class);
         //$logger = new \Tests\Michaelc\Voting\TestLogger();
 
-        $runner = new VoteHandler($election, $logger);
-        $runner->run();
+        $handler = new VoteHandler($election, $logger);
+        //$handler->run();
+    }
+
+    public function testBallotValidity()
+    {
+        $election = StvElectionTest::getSampleElection();
+        $logger = $this->createMock(Logger::class);
+
+        $handler = new VoteHandler($election, $logger);
+
+        $method = new \ReflectionMethod('\Michaelc\Voting\STV\VoteHandler', 'checkBallotValidity');
+
+        $method->setAccessible(TRUE);
+        $ballot = new Ballot([4, 5, 8]);
+        $this->assertTrue($method->invoke($handler, $ballot));
+
+        $ballot = new Ballot([-1, 5, 8]);
+        $this->assertFalse($method->invoke($handler, $ballot));
+
+        $ballot = new Ballot([1, 5, 22]);
+        $this->assertFalse($method->invoke($handler, $ballot));
+
+        $ballot = new Ballot([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 18, 17]);
+        $this->assertFalse($method->invoke($handler, $ballot));
     }
 }
